c67b6691260accb7b7ee8aae8e97ecbe259f7741
manish veerwal <manish.veerwal@gmail.com>
2013-06-18 14:37:10 +0530

    added model attribute LogonBean

--- ExpenseTracker/src/main/java/org/expense/spring/mvc/controller/WelcomeController.java
+++ ExpenseTracker/src/main/java/org/expense/spring/mvc/controller/WelcomeController.java
@@ -1,7 +1,9 @@
 package org.expense.spring.mvc.controller;
 
 import org.apache.log4j.Logger;
+import org.expense.spring.mvc.javabeans.LogonBean;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
 
@@ -11,7 +13,10 @@ public class WelcomeController {
 	private static final Logger log = Logger.getLogger(WelcomeController.class);
 	
 	@RequestMapping(value={"/", "/home"}, method=GET)
-	public String showWelcomePage(){
+	public String showWelcomePage(Model model){
+		
+		//add LogonBean for modelAttribute
+		model.addAttribute(new LogonBean());
 		return "index";
 	}
 	
