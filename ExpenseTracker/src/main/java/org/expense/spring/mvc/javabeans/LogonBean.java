af7fd35c4e57ee2d7e1027c43be0cf046ba75828
manish veerwal <manish.veerwal@gmail.com>
2013-06-18 16:52:57 +0530

    validate email address

--- ExpenseTracker/src/main/java/org/expense/spring/mvc/javabeans/LogonBean.java
+++ ExpenseTracker/src/main/java/org/expense/spring/mvc/javabeans/LogonBean.java
@@ -1,10 +1,13 @@
 package org.expense.spring.mvc.javabeans;
 
+import javax.validation.constraints.Pattern;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 public class LogonBean {
 	
 	@NotEmpty
+	@Pattern(regexp="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}", message="Invalid email address")
 	private String email;
 	
 	@NotEmpty
