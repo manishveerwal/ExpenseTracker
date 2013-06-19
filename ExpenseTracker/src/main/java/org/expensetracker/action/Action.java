0fe37956ee02b0d243b1351ac3297563882cd930
manish veerwal <manish.veerwal@gmail.com>
2013-06-19 16:00:32 +0530

    removed old action package and its classes.

--- ExpenseTracker/src/main/java/org/expensetracker/action/Action.java
+++
@@ -1,8 +0,0 @@
-package org.expensetracker.action;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface Action {
-	public void execute(HttpServletRequest request, HttpServletResponse response);
-}
